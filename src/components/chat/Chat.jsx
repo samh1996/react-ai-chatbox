505a1aa355984a1934fa4719a035504a01a3a855
samh1996 <27236262+samh1996@users.noreply.github.com>
2026-01-26 19:43:20 -0500

    Added support for dark and light mode - add a button later

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,20 +1,46 @@
+import { useEffect, useRef, useMemo } from "react";
 import styles from "./Chat.module.css";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
-const WELCOME_MESSAGE = {
-  role: "assistant",
-  content: "Hello! I'm your AI Chatbot. How can I assist you today?",
-};
+const WELCOME_MESSAGE_GROUP = [
+  {
+    role: "assistant",
+    content: "Hello! I'm your AI Chatbot. How can I assist you today?",
+  },
+];
 
 export function Chat({ messages }) {
+  const messagesEndRef = useRef(null);
+  const messageGroups = useMemo(
+    () =>
+      messages.reduce((groups, message) => {
+        if (message.role === "user") groups.push([]);
+        groups[groups.length - 1].push(message);
+        return groups;
+      }, []),
+    [messages],
+  );
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className={styles.Chat}>
-      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-        <div key={index} data-role={role} className={styles.Message}>
-          <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+      {[WELCOME_MESSAGE_GROUP, ...messageGroups].map((messages, groupIndex) => (
+        // Group
+        <div key={groupIndex} className={styles.Group}>
+          {messages.map(({ role, content }, index) => (
+            // Message
+            <div key={index} className={styles.Message} data-role={role}>
+              <Markdown>{content}</Markdown>
+            </div>
+          ))}
         </div>
       ))}
+
+      <div ref={messagesEndRef} />
     </div>
   );
 }
